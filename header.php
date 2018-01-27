0bfde0adcb0edc6956d96d19399440573e3fd734
JasonKhew96 <29531167+JasonKhew96@users.noreply.github.com>
2018-01-27 23:33:38 +0800

    Bootstrap and php testing.

---
+++ header.php
@@ -0,0 +1,22 @@
+<div class="container">
+  <!-- Logo -->
+  <div class="py-3 text-center">
+    <img class="d-block mx-auto mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+    <h2>Tindustries</h2>
+  </div>
+  <!-- Navigation bar -->
+  <ul class="nav nav-tabs justify-content-center">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+  </ul>
+</div>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+  <title>Index</title>
+</head>
+
+<body>
+  <?php include('header.php'); ?>
+
+  <script src="js/jquery-3.3.1.slim.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
